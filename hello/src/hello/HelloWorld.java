d369801c3747daa7327e3d0894c1f0f2ba80eba2
Mark Jenkins <markjenkinsjobs@gmail.com>
2021-01-23 10:18:19 -0500

    BMW-122: Added Java Simple Project

---
+++ hello/src/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package hello;
+
+/**
+ * @author markb
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println( "Hello, my name is Mark Boahen" );
+	}
+
+}
