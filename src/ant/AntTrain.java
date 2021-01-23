edac0d2e03338660224cbe1ab81cc7cbc182a1ce
Mark Jenkins <markjenkinsjobs@gmail.com>
2021-01-23 13:27:01 -0500

    BMW-122: Changes Added

---
+++ src/ant/AntTrain.java
@@ -0,0 +1,13 @@
+package ant;
+
+public class AntTrain {
+
+	public static void main(String[] args) 
+	{
+	  //Properties prop = System.getProperties();
+	  //Enumeration<?> en = prop.propertyNames(); 
+	  System.out.println("I am out here in ant build!");
+
+	}
+
+}
